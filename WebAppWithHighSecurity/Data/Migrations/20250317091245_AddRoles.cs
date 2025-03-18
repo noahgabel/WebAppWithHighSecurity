6fe114114571e34388a389eada208b743d7b0214
Noah Gabel <noahgabel1012@gmail.com>
2025-03-18 19:57:47 +0100

    Migrations

---
+++ WebAppWithHighSecurity/Data/Migrations/20250317091245_AddRoles.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAppWithHighSecurity.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[,]
+                {
+                    { "1", "Admin", "ADMIN", Guid.NewGuid().ToString() },
+                    { "2", "User", "USER", Guid.NewGuid().ToString() },
+                    { "3", "Manager", "MANAGER", Guid.NewGuid().ToString() }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3");
+        }
+    }
+}
---
+++ WebAppWithHighSecurity/Data/Migrations/20250317100818_JohnsLangeFinger.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAppWithHighSecurity.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class JohnsLangeFinger : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserCprNumbers_ApplicationUser_UserId1",
+                table: "UserCprNumbers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserCprNumbers_UserId1",
+                table: "UserCprNumbers");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "UserCprNumbers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "UserCprNumbers",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCprNumbers_UserId1",
+                table: "UserCprNumbers",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserCprNumbers_ApplicationUser_UserId1",
+                table: "UserCprNumbers",
+                column: "UserId1",
+                principalTable: "ApplicationUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
